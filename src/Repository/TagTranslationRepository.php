5eb9ea2422bc3473e54d6b3ba069a2021b7afde7
Rebstew <w3b5t3r56@gmail.com>
2020-08-25 18:19:58 +0200

    fix tags feature from comments on https://github.com/Drouizig/displeger-web/pull/41

--- src/Repository/TagTranslationRepository.php
+++ src/Repository/TagTranslationRepository.php
@@ -18,33 +18,4 @@ class TagTranslationRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, TagTranslation::class);
     }
-
-    // /**
-    //  * @return TagTranslation[] Returns an array of TagTranslation objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('t.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?TagTranslation
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
