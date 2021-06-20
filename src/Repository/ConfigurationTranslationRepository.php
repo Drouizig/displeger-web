7de1c5a7a5582116b39d0cac54d3e1ee06b6e206
Gwenn M <tornoz@gozmail.bzh>
2021-06-20 14:32:17 +0200

    C update dependencie

--- src/Repository/ConfigurationTranslationRepository.php
+++ src/Repository/ConfigurationTranslationRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\ConfigurationTranslation;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method ConfigurationTranslation|null find($id, $lockMode = null, $lockVersion = null)
